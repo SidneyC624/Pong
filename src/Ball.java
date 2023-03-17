af103047948bace3b95d457ae7e39314e7aff982
SidneyC624 <sidney.0503c@gmail.com>
2023-03-17 19:10:58 +0800

    setup methods and attributes

---
+++ src/Ball.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+	Random random;
+	int XVelocity;
+	int YVelocity;
+	Ball() {
+		
+	}
+	
+	public void setXDirection(int randomXDirection) {
+		
+	}
+	
+	public void setYDirection(int randomYDirection) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+}
